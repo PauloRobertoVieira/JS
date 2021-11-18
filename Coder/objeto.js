68052642ffbee4bd047535993a4391d7691e199c
PRVieira <paulovieira@discente.ufg.br>
2021-11-18 16:07:02 -0300

    "criado vários objetos"

--- Coder/objeto.js
+++ Coder/objeto.js
@@ -8,3 +8,25 @@ console.log("***************")
 class Produto{}
 console.log(typeof Produto)
 console.log(typeof new Produto())
+
+// par chave/valor
+const saudacao = 'Olá' // contexto lexico
+
+function exec(){
+    const saudacao = 'E ai blz?' // outro contexto lexico
+    return saudacao
+}
+// Objetos são grupos aninhados de pares nome/valor
+const cliente = {
+    nome: 'Eva',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: "Rua felicidade",
+        numero: 13
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
