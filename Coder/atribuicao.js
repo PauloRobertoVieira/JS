95494f78301cf04bb5f40705de10a36b4b5b7fac
PRVieira <paulovieira@discente.ufg.br>
2021-11-18 16:22:23 -0300

    operador de atribuições

---
+++ Coder/atribuicao.js
@@ -0,0 +1,18 @@
+const a = 7
+
+let b = 3
+
+b += a
+console.log("soma 7+3=",b)
+
+b -= 2
+console.log('subtração de 10-2=', b)
+
+b *= 2
+console.log('multiplicação de 8 * 2=', b)
+
+b /= 4
+console.log('divisão de 16 / 4=', b)
+
+b %= 3
+console.log('resto da divisão de 4/3=', b)
