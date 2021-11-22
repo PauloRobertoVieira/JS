8591bd3c29501d3ca2b3a6de69d2dba7ab55cace
PRVieira <paulovieira@discente.ufg.br>
2021-11-22 14:33:06 -0300

    operadores aritmeticos

---
+++ Coder/aritmeticos.js
@@ -0,0 +1,13 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = b+c
+console.log("soma 5+1 =", soma)
+
+const sub = b-a
+console.log("subtação de 5-3 =", sub)
+
+const mult = a*b
+console.log("multiplicação de 3*5 =", mult)
+
+const div = d/a
+console.log("divisão de 15/3 =", div)
