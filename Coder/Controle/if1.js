1fe72cc9c9fb20db28f077e243c2ce27cf5599cf
PRVieira <paulovieira@discente.ufg.br>
2021-11-23 15:22:41 -0300

    estrutura condição if e else

--- Coder/Controle/if1.js
+++ Coder/Controle/if1.js
@@ -1,1 +1,25 @@
-function soBoaNoticia
+function soBoaNoticia(nota) {
+  if (nota >= 7) {
+      console.log('Aprovado com '+nota)
+  }
+}
+
+soBoaNoticia(8.4)
+soBoaNoticia(6.8)
+
+function seForVerdadeEuFalo(valor){
+    if(valor) {
+        console.log('É verdade...'+valor)
+    }
+}
+
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+//seForVerdadeEuFalo(Nan)
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(0)
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo([1, 2])
+seForVerdadeEuFalo({})
