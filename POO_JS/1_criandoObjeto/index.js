6163a77edf6d394cc1f267e219694cb1b7d2560d
PRVieira <paulovieira@discente.ufg.br>
2021-11-24 14:57:09 -0300

    Objetos em POO

---
+++ POO_JS/1_criandoObjeto/index.js
@@ -0,0 +1,16 @@
+let pessoa = {
+    nome: 'Eva',
+    idade: 5
+}
+
+console.log(pessoa)
+console.log(pessoa.nome)
+
+let carro = {
+  cor: null,
+  portas: null,
+};
+
+carro.cor = prompt("Informe a cor");
+console.log(carro.cor)
+alert(carro.cor);
